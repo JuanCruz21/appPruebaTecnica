305d22daf643a8f6b4e6dd4cb40776c766c78c8f
JuanCruz21 <juancruz.02.2001@gmail.com>
2024-11-18 11:22:22 -0500

    Correcciones

--- api/routes/api.php
+++ api/routes/api.php
@@ -12,10 +12,10 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::prefix('v1')->group(function () {
+Route::prefix('v1')->middleware('auth:sanctum')->group(function () {
     Route::apiResource('/content', ContentController::class);
     Route::apiResource('/category', CategoryController::class);
-})->middleware('auth:sanctum');
+});
 
 Route::prefix('Auth')->group(function () {
     Route::post('/login', LoginController::class);
